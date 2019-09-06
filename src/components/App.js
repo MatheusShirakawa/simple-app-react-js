02e96b75478fe5475728bbcb94a9ab772e41b9ca
MatheusShirakawa <matheus_shirakawa@outlook.com>
2019-09-05 22:52:33 -0300

    changing test message

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ export default class App extends React.Component{
     render(){
         return (
             <div>
-                <h1>Hello World</h1>  
-                <Post title="loucura" />
-                <Post title="teste 2443423" />
-                <Post title="meu deus" />
+                <h1>Hello World</h1>
+                <Post title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut " />
+                <Post title=" labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " />
+                <Post title="sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
             </div>
         );
 
